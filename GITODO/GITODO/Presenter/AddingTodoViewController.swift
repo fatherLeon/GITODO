7bbe434baceba96cac9bcfaa08db9d9b0142d48f
minsu3007 <minsu3007@gmail.com>
2023-08-10 14:23:27 +0900

    docs: create AddingTodoViewController

---
+++ GITODO/GITODO/Presenter/AddingTodoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddingTodoViewController.swift
+//  GITODO
+//
+//  Created by 강민수 on 2023/08/10.
+//
+
+import UIKit
+
+class AddingTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
