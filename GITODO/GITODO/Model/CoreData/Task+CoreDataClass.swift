22b49c7cad4b5352f299bca9a0eb3b883e065daa
minsu3007 <minsu3007@gmail.com>
2023-08-11 13:47:09 +0900

    docs: add CoreData Entity

---
+++ GITODO/GITODO/Model/CoreData/Task+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Task+CoreDataClass.swift
+//  GITODO
+//
+//  Created by 강민수 on 2023/08/11.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
---
+++ GITODO/GITODO/Service/CoreDataManager.swift
@@ -0,0 +1,12 @@
+//
+//  CoreDataManager.swift
+//  GITODO
+//
+//  Created by 강민수 on 2023/08/11.
+//
+
+import Foundation
+
+final class CoreDataManager {
+    
+}
