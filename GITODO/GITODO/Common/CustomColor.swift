c180bafd89a8ef22f8dec9490d87c4a53de78e02
minsu3007 <minsu3007@gmail.com>
2023-09-01 15:58:25 +0900

    feat: add CustomColorSet

--- GITODO/GITODO/Common/CustomColor.swift
+++ GITODO/GITODO/Common/CustomColor.swift
@@ -10,4 +10,6 @@ import UIKit
 enum CustomColor {
     static var CommitColorSet = CustomColor.GreenColorSet
     static let GreenColorSet = [UIColor(named: "BasicCommitGreen1"), UIColor(named: "BasicCommitGreen2"), UIColor(named: "BasicCommitGreen3")]
+    static let SkyColorSet = [UIColor(named: "BasicCommitSky1"), UIColor(named: "BasicCommitSky2"), UIColor(named: "BasicCommitSky3")]
+    static let RedColorSet = [UIColor(named: "BasicCommitRed1"), UIColor(named: "BasicCommitRed2"), UIColor(named: "BasicCommitRed3")]
 }
